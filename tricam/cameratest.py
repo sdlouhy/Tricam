f66c00ad46e059631dc439b00f909950a0382f13
sdlouhy <sarah.dlouhy.0113@gmail.com>
2019-03-15 14:14:26 -0500

    formatting the test file

--- tricam/cameratest.py
+++ tricam/cameratest.py
@@ -1,5 +1,12 @@
 from tricam.stereo_cameras import StereoGroup as StereoGroup
+from tricam.stereo_cameras import ChessboardFinder as ChessboardFinder
 
 devices = (1, 2, 3)
 tricam = StereoGroup(devices)
 tricam.show_videos()
+
+# calibrator test
+calib = ChessboardFinder(tricam)
+
+
+# point cloud test
